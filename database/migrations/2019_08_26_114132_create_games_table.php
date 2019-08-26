988e982c94e97789d3eab4f122d8a480a5a136c7
Goran Hrženjak <ghoc.hrx@gmail.com>
2019-08-26 16:56:42 +0200

    Migrations and Model classes for Game-related models

---
+++ database/migrations/2019_08_26_114132_create_games_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('home_team_id')->nullable()->index();
+            $table->unsignedBigInteger('away_team_id')->nullable()->index();
+            $table->foreign('home_team_id')->references('id')->on('teams')->onDelete('set null');
+            $table->foreign('away_team_id')->references('id')->on('teams')->onDelete('set null');
+
+            $table->unsignedBigInteger('season_id')->nullable()->index();
+            $table->foreign('season_id')->references('id')->on('seasons')->onDelete('set null');
+
+            $table->unsignedBigInteger('competition_id')->nullable()->index();
+            $table->foreign('competition_id')->references('id')->on('competitions')->onDelete('set null');
+
+            $table->integer('round')->nullable();
+
+            $table->dateTime('datetime')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
