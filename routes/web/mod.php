cd0a492cc4cb8ea56f4e907ef19ef9460b538ba9
Goran Hrženjak <ghoc.hrx@gmail.com>
2019-10-01 11:21:11 +0200

    Routes declared differently to cater for IDE/plugin auto-suggestions

--- routes/web/mod.php
+++ routes/web/mod.php
@@ -1,23 +1,24 @@
 <?php
 
-Route::prefix('mod')
-    ->name('mod.')
-    ->middleware(['auth', 'role:super_admin,admin,mod'])
-    ->group(function () {
-        // Routes are prefixed with /mod        -- e.g. mod/players/5/edit
-        // Route names are prefixed with mod.   -- e.g. mod.games.index
+Route::group(['prefix' => 'mod', 'middleware' => ['auth', 'role:super_admin,admin,mod'], 'as' => 'mod.'], function () {
+    // Routes are prefixed with /mod        -- e.g. mod/players/5/edit
+    // Route names are prefixed with mod.   -- e.g. mod.games.index
 
-        // Controllers within the "App\Http\Controllers\Mod" namespace
-        Route::resource('teams', 'Mod\TeamController')->except('show');
+    // Controllers within the "App\Http\Controllers\Mod" namespace
+    Route::resource('teams', 'Mod\TeamController')->except('show');
 
-        Route::resource('games', 'Mod\GameController')->except('show');
-        Route::get('games/{game}/result')
-            ->uses('Mod\GameController@editResult')
-            ->name('games.result.edit');
-        Route::patch('games/{game}/result')
-            ->uses('Mod\GameController@updateResult')
-            ->name('games.result.update');
+    Route::resource('games', 'Mod\GameController')->except('show');
+    Route::get('games/{game}/result', [
+        'uses' => 'Mod\GameController@editResult',
+        'as'   => 'games.result.edit',
+    ]);
 
-        Route::resource('players', 'Mod\PlayerController')->except('show');
+    Route::patch('games/{game}/result', [
+        'uses' => 'Mod\GameController@updateResult',
+        'as'   => 'games.result.update',
+    ]);
 
-    });
+
+    Route::resource('players', 'Mod\PlayerController')->except('show');
+
+});
