5d49cd405cabdc7f6aaabc5d1044f6b7c9558ebe
Gh0C <ghoc.hrx@gmail.com>
2020-07-31 10:57:36 +0200

    A whitespace added in HTML markup

--- resources/views/home/display-partials/team.blade.php
+++ resources/views/home/display-partials/team.blade.php
@@ -1,7 +1,7 @@
 <div class="row text-center">
     @if($team->featured_image)
         <div class="col-6 offset-3 p-0">
-            <img src="{{ $team->logoUrl() }}" alt="{{ $team->name }}"class="game-team-logo">
+            <img src="{{ $team->logoUrl() }}" alt="{{ $team->name }}" class="game-team-logo">
         </div>
     @endif
 </div>
